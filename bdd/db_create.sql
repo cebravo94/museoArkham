68a52779f1693e02684c3ba72f2a6b9f089f8ba2
Unknown <maxhg.96@gmail.com>
2018-05-07 19:06:21 -0300

    ScriptSql
    Revisado con el diagrama EER propuesto.

--- bdd/db_create.sql
+++ bdd/db_create.sql
@@ -2,26 +2,26 @@ CREATE DATABASE IF NOT EXISTS museo;
 
 USE museo;
 
-DROP TABLE IF EXISTS administrador;
+DROP TABLE IF EXISTS usuario;
 
-CREATE TABLE administrador(
-	id_admin INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	nombre_usuario VARCHAR(63) UNIQUE NOT NULL,
+CREATE TABLE usuario(
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	contrasenna VARCHAR(16) NOT NULL,
 	nombre VARCHAR(63) NOT NULL,
 	rut VARCHAR(9) NOT NULL,
 	correo VARCHAR(63) UNIQUE NOT NULL,
-	fecha_ingreso DATE NOT NULL
+	fecha_ingreso DATE NOT NULL,
+	tipo VARCHAR(63) NOT NULL
 );
 
 DROP TABLE IF EXISTS departamento;
 
 CREATE TABLE departamento(
 	id_dpto INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	id_admin INT NOT NULL,
+	id_usuario INT NOT NULL,
 	nombre VARCHAR(63) NOT NULL,
 	descripcion VARCHAR(63),
-	FOREIGN KEY (id_admin) REFERENCES administrador(id_admin) ON DELETE RESTRICT
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE RESTRICT
 );
 
 DROP TABLE IF EXISTS sala;
@@ -41,6 +41,7 @@ CREATE TABLE item(
 	id_item INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	id_dpto INT NOT NULL,
 	id_sala INT NOT NULL,
+	nombre VARCHAR(63),
 	fecha_ingreso DATE NOT NULL,
 	descripcion VARCHAR(63),
 	coleccion VARCHAR(63),
@@ -49,20 +50,37 @@ CREATE TABLE item(
 	FOREIGN KEY (id_sala) REFERENCES sala(id_sala) ON DELETE RESTRICT
 );
 
+DROP TABLE IF EXISTS solicitud;
+
+CREATE TABLE solicitud(
+	id_solicitud INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	id_dpto INT NOT NULL,
+	id_item INT NOT NULL,
+	id_usuario INT NOT NULL,
+	id_sala_origen INT NOT NULL,
+	id_sala_destino INT NOT NULL,
+	estado VARCHAR(63) NOT NULL,
+	comentario VARCHAR(63),
+	FOREIGN KEY (id_dpto) REFERENCES departamento(id_dpto) ON DELETE RESTRICT,
+	FOREIGN KEY (id_item) REFERENCES item(id_item) ON DELETE RESTRICT,
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE RESTRICT,
+	FOREIGN KEY (id_sala_origen) REFERENCES sala(id_sala) ON DELETE RESTRICT,
+	FOREIGN KEY (id_sala_destino) REFERENCES sala(id_sala) ON DELETE RESTRICT
+);
+
 DROP TABLE IF EXISTS registro;
 
 CREATE TABLE registro(
 	id_registro INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	id_dpto INT NOT NULL,
 	id_item INT NOT NULL,
-	id_admin INT NOT NULL,
+	id_usuario INT NOT NULL,
 	id_sala_origen INT NOT NULL,
 	id_sala_destino INT NOT NULL,
 	fecha_ingreso DATE NOT NULL,
-	observacuin VARCHAR(63),
 	FOREIGN KEY (id_dpto) REFERENCES departamento(id_dpto) ON DELETE RESTRICT,
 	FOREIGN KEY (id_item) REFERENCES item(id_item) ON DELETE RESTRICT,
-	FOREIGN KEY (id_admin) REFERENCES administrador(id_admin) ON DELETE RESTRICT,
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE RESTRICT,
 	FOREIGN KEY (id_sala_origen) REFERENCES sala(id_sala) ON DELETE RESTRICT,
 	FOREIGN KEY (id_sala_destino) REFERENCES sala(id_sala) ON DELETE RESTRICT
 );
