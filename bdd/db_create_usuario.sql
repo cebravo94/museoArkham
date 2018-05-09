39a1f10da25e8b2c03df0121d4a5fb56b9b0a3e2
César Bravo <cebravo94@gmail.com>
2018-05-09 01:27:43 -0300

    Scripts arreglados

---
+++ bdd/db_create_usuario.sql
@@ -0,0 +1,8 @@
+USE museo;
+
+INSERT INTO Usuario (contrasenna,nombre,rut,correo,fecha_ingreso,tipo) VALUES (1234,'Juana Maria',11658123-8,'juanamaria@gmail.com',14/05/1987,'Secretaria');
+INSERT INTO Usuario (contrasenna,nombre,rut,correo,fecha_ingreso,tipo) VALUES (1234,'Juan Letelier',23658123-8,'juanletelier@gmail.com',14/06/9287,'Bodega');
+INSERT INTO Usuario (contrasenna,nombre,rut,correo,fecha_ingreso,tipo) VALUES (1234,'Miguel Gonzalez',18563987-1,'miguelgonzalez@gmail.com',13/05/2000,'Director');
+INSERT INTO Usuario (contrasenna,nombre,rut,correo,fecha_ingreso,tipo) VALUES (1234,'Rodrigo Cordero',19696147-1,'rodrigocordero@gmail.com',12/07/2005,'Gerente');
+INSERT INTO Usuario (contrasenna,nombre,rut,correo,fecha_ingreso,tipo) VALUES (1234,'Jesus Moris',176961472-1,'jesusmoris@gmail.com',13/04/2010,'Secretaria');
+INSERT INTO Usuario (contrasenna,nombre,rut,correo,fecha_ingreso,tipo) VALUES (1234,'Bastian Sepulveda',19563387-1,'bastiansepulveda@gmail.com',17/03/1996,'Administrador');
