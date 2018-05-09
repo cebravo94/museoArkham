81627e759b0e827dfdc84e374d363b664c4b4b33
César Bravo <cebravo94@gmail.com>
2018-05-09 00:59:17 -0300

    Conector BDD v0.1 y Controlador LogIn básico

---
+++ MuseoArkham/Controlador/ControladorLogIn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MuseoArkham.Controlador
+{
+    class ControladorLogIn : Controlador
+    {
+
+        public bool verificarUsuario(string usuario, string contrasenna) {
+            MySqlDataReader reader = this.realizarConsulta(usuario);
+            if (reader != null) {
+                reader.Read();
+                if (reader["contrasenna"].ToString().Equals(contrasenna)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
