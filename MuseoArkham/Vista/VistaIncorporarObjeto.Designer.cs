842a19b49e93ad23e44361055c147c07b9370095
Feñaña <flopez14@alumnos.utalca.cl>
2018-05-09 01:49:16 -0300

    Creación vista incorporar objetos

---
+++ MuseoArkham/Vista/VistaIncorporarObjeto.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace MuseoArkham.Vista
+{
+    partial class VistaIncorporarObjeto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VistaIncorporarObjeto));
+            this.groupBoxIncorporarObjeto = new System.Windows.Forms.GroupBox();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.comboBoxTipoObjeto = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTipoDocumento = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelAutorDocumento = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxMaterial = new System.Windows.Forms.TextBox();
+            this.labelMaterial = new System.Windows.Forms.Label();
+            this.textBoxEstilo = new System.Windows.Forms.TextBox();
+            this.labelEstilo = new System.Windows.Forms.Label();
+            this.comboBoxTipoObra = new System.Windows.Forms.ComboBox();
+            this.labelTipoObra = new System.Windows.Forms.Label();
+            this.textBoxPeriodo = new System.Windows.Forms.TextBox();
+            this.labelPeriodo = new System.Windows.Forms.Label();
+            this.textBoxMarca = new System.Windows.Forms.TextBox();
+            this.textBoxModelo = new System.Windows.Forms.TextBox();
+            this.textBoxAnno = new System.Windows.Forms.TextBox();
+            this.labelMarca = new System.Windows.Forms.Label();
+            this.labelModelo = new System.Windows.Forms.Label();
+            this.labelAnno = new System.Windows.Forms.Label();
+            this.groupBoxIncorporarObjeto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxIncorporarObjeto
+            // 
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelAnno);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelModelo);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelMarca);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBoxAnno);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBoxModelo);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBoxMarca);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelPeriodo);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBoxPeriodo);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelTipoObra);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.comboBoxTipoObra);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelEstilo);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBoxEstilo);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelMaterial);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBoxMaterial);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBox2);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelAutorDocumento);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.comboBox1);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.labelTipoDocumento);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.label1);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.comboBoxTipoObjeto);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.label3);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBox1);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.label2);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.textBoxNombre);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.buttonAceptar);
+            this.groupBoxIncorporarObjeto.Controls.Add(this.buttonCancelar);
+            this.groupBoxIncorporarObjeto.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxIncorporarObjeto.Name = "groupBoxIncorporarObjeto";
+            this.groupBoxIncorporarObjeto.Size = new System.Drawing.Size(310, 387);
+            this.groupBoxIncorporarObjeto.TabIndex = 0;
+            this.groupBoxIncorporarObjeto.TabStop = false;
+            this.groupBoxIncorporarObjeto.Text = "Incorporar objeto";
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(229, 358);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 0;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Location = new System.Drawing.Point(148, 358);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAceptar.TabIndex = 1;
+            this.buttonAceptar.Text = "Incorporar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(142, 46);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(162, 20);
+            this.textBoxNombre.TabIndex = 2;
+            // 
+            // comboBoxTipoObjeto
+            // 
+            this.comboBoxTipoObjeto.FormattingEnabled = true;
+            this.comboBoxTipoObjeto.Location = new System.Drawing.Point(142, 19);
+            this.comboBoxTipoObjeto.Name = "comboBoxTipoObjeto";
+            this.comboBoxTipoObjeto.Size = new System.Drawing.Size(162, 21);
+            this.comboBoxTipoObjeto.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tipo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 72);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 44);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Descripción";
+            // 
+            // labelTipoDocumento
+            // 
+            this.labelTipoDocumento.AutoSize = true;
+            this.labelTipoDocumento.Location = new System.Drawing.Point(44, 123);
+            this.labelTipoDocumento.Name = "labelTipoDocumento";
+            this.labelTipoDocumento.Size = new System.Drawing.Size(84, 13);
+            this.labelTipoDocumento.TabIndex = 9;
+            this.labelTipoDocumento.Text = "Tipo documento";
+            this.labelTipoDocumento.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(142, 123);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(162, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // labelAutorDocumento
+            // 
+            this.labelAutorDocumento.AutoSize = true;
+            this.labelAutorDocumento.Location = new System.Drawing.Point(96, 150);
+            this.labelAutorDocumento.Name = "labelAutorDocumento";
+            this.labelAutorDocumento.Size = new System.Drawing.Size(32, 13);
+            this.labelAutorDocumento.TabIndex = 11;
+            this.labelAutorDocumento.Text = "Autor";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBoxMaterial
+            // 
+            this.textBoxMaterial.Location = new System.Drawing.Point(142, 176);
+            this.textBoxMaterial.Name = "textBoxMaterial";
+            this.textBoxMaterial.Size = new System.Drawing.Size(162, 20);
+            this.textBoxMaterial.TabIndex = 13;
+            // 
+            // labelMaterial
+            // 
+            this.labelMaterial.AutoSize = true;
+            this.labelMaterial.Location = new System.Drawing.Point(84, 176);
+            this.labelMaterial.Name = "labelMaterial";
+            this.labelMaterial.Size = new System.Drawing.Size(44, 13);
+            this.labelMaterial.TabIndex = 14;
+            this.labelMaterial.Text = "Material";
+            // 
+            // textBoxEstilo
+            // 
+            this.textBoxEstilo.Location = new System.Drawing.Point(142, 202);
+            this.textBoxEstilo.Name = "textBoxEstilo";
+            this.textBoxEstilo.Size = new System.Drawing.Size(162, 20);
+            this.textBoxEstilo.TabIndex = 15;
+            // 
+            // labelEstilo
+            // 
+            this.labelEstilo.AutoSize = true;
+            this.labelEstilo.Location = new System.Drawing.Point(96, 202);
+            this.labelEstilo.Name = "labelEstilo";
+            this.labelEstilo.Size = new System.Drawing.Size(32, 13);
+            this.labelEstilo.TabIndex = 16;
+            this.labelEstilo.Text = "Estilo";
+            // 
+            // comboBoxTipoObra
+            // 
+            this.comboBoxTipoObra.FormattingEnabled = true;
+            this.comboBoxTipoObra.Location = new System.Drawing.Point(142, 228);
+            this.comboBoxTipoObra.Name = "comboBoxTipoObra";
+            this.comboBoxTipoObra.Size = new System.Drawing.Size(162, 21);
+            this.comboBoxTipoObra.TabIndex = 17;
+            // 
+            // labelTipoObra
+            // 
+            this.labelTipoObra.AutoSize = true;
+            this.labelTipoObra.Location = new System.Drawing.Point(76, 228);
+            this.labelTipoObra.Name = "labelTipoObra";
+            this.labelTipoObra.Size = new System.Drawing.Size(52, 13);
+            this.labelTipoObra.TabIndex = 18;
+            this.labelTipoObra.Text = "Tipo obra";
+            // 
+            // textBoxPeriodo
+            // 
+            this.textBoxPeriodo.Location = new System.Drawing.Point(142, 255);
+            this.textBoxPeriodo.Name = "textBoxPeriodo";
+            this.textBoxPeriodo.Size = new System.Drawing.Size(162, 20);
+            this.textBoxPeriodo.TabIndex = 19;
+            // 
+            // labelPeriodo
+            // 
+            this.labelPeriodo.AutoSize = true;
+            this.labelPeriodo.Location = new System.Drawing.Point(85, 255);
+            this.labelPeriodo.Name = "labelPeriodo";
+            this.labelPeriodo.Size = new System.Drawing.Size(43, 13);
+            this.labelPeriodo.TabIndex = 20;
+            this.labelPeriodo.Text = "Periodo";
+            // 
+            // textBoxMarca
+            // 
+            this.textBoxMarca.Location = new System.Drawing.Point(142, 281);
+            this.textBoxMarca.Name = "textBoxMarca";
+            this.textBoxMarca.Size = new System.Drawing.Size(162, 20);
+            this.textBoxMarca.TabIndex = 21;
+            // 
+            // textBoxModelo
+            // 
+            this.textBoxModelo.Location = new System.Drawing.Point(142, 307);
+            this.textBoxModelo.Name = "textBoxModelo";
+            this.textBoxModelo.Size = new System.Drawing.Size(162, 20);
+            this.textBoxModelo.TabIndex = 22;
+            // 
+            // textBoxAnno
+            // 
+            this.textBoxAnno.Location = new System.Drawing.Point(142, 333);
+            this.textBoxAnno.Name = "textBoxAnno";
+            this.textBoxAnno.Size = new System.Drawing.Size(162, 20);
+            this.textBoxAnno.TabIndex = 23;
+            // 
+            // labelMarca
+            // 
+            this.labelMarca.AutoSize = true;
+            this.labelMarca.Location = new System.Drawing.Point(91, 281);
+            this.labelMarca.Name = "labelMarca";
+            this.labelMarca.Size = new System.Drawing.Size(37, 13);
+            this.labelMarca.TabIndex = 24;
+            this.labelMarca.Text = "Marca";
+            // 
+            // labelModelo
+            // 
+            this.labelModelo.AutoSize = true;
+            this.labelModelo.Location = new System.Drawing.Point(86, 307);
+            this.labelModelo.Name = "labelModelo";
+            this.labelModelo.Size = new System.Drawing.Size(42, 13);
+            this.labelModelo.TabIndex = 25;
+            this.labelModelo.Text = "Modelo";
+            // 
+            // labelAnno
+            // 
+            this.labelAnno.AutoSize = true;
+            this.labelAnno.Location = new System.Drawing.Point(102, 333);
+            this.labelAnno.Name = "labelAnno";
+            this.labelAnno.Size = new System.Drawing.Size(26, 13);
+            this.labelAnno.TabIndex = 26;
+            this.labelAnno.Text = "Año";
+            // 
+            // VistaIncorporarObjeto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 411);
+            this.Controls.Add(this.groupBoxIncorporarObjeto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "VistaIncorporarObjeto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Incorporar objeto";
+            this.Load += new System.EventHandler(this.VistaAgregarObjeto_Load);
+            this.groupBoxIncorporarObjeto.ResumeLayout(false);
+            this.groupBoxIncorporarObjeto.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxIncorporarObjeto;
+        private System.Windows.Forms.Button buttonAceptar;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Label labelTipoDocumento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxTipoObjeto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelAnno;
+        private System.Windows.Forms.Label labelModelo;
+        private System.Windows.Forms.Label labelMarca;
+        private System.Windows.Forms.TextBox textBoxAnno;
+        private System.Windows.Forms.TextBox textBoxModelo;
+        private System.Windows.Forms.TextBox textBoxMarca;
+        private System.Windows.Forms.Label labelPeriodo;
+        private System.Windows.Forms.TextBox textBoxPeriodo;
+        private System.Windows.Forms.Label labelTipoObra;
+        private System.Windows.Forms.ComboBox comboBoxTipoObra;
+        private System.Windows.Forms.Label labelEstilo;
+        private System.Windows.Forms.TextBox textBoxEstilo;
+        private System.Windows.Forms.Label labelMaterial;
+        private System.Windows.Forms.TextBox textBoxMaterial;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelAutorDocumento;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
