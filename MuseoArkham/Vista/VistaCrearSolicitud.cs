ffcc910dd541c3da133d8987fe5d9ebb0507b551
Feñaña <flopez14@alumnos.utalca.cl>
2018-05-09 01:45:28 -0300

    Vista crear solicitudes

---
+++ MuseoArkham/Vista/VistaCrearSolicitud.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuseoArkham.Vista
+{
+    public partial class VistaCrearSolicitud : Form
+    {
+        public VistaCrearSolicitud()
+        {
+            InitializeComponent();
+        }
+
+        private void CrearSolicitud_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
