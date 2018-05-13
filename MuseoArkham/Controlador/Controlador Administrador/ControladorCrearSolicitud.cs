50a279c7199f9be740387200aa50e68b96c66e59
raimundovu <rvasquez15@alumnos.utalca.cl>
2018-05-13 01:03:34 -0400

    Creación Controlador Solicitudes
    Se crea un Controlador para la ventana de creación de solicitudes, con los métodos correspondientes a esta ventana.

---
+++ MuseoArkham/Controlador/Controlador Administrador/ControladorCrearSolicitud.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MuseoArkham.Vista;
+using MuseoArkham.Modelo;
+
+namespace MuseoArkham.Controlador.Controlador_Administrador
+{
+    class ControladorCrearSolicitud : Controlador
+    {
+        private VistaCrearSolicitud ventana;
+        Solicitud SolicitudNueva { get; set; }
+
+        public ControladorCrearSolicitud(VistaCrearSolicitud ventana)
+        {
+            this.ventana = ventana;
+        }
+
+        /**
+         * <summary>
+         * Este metodo realiza las acciones correspondientes al boton aceptar
+         * </summary>
+         * <param name="consulta"> Es la consulta SQL</param>
+         * 
+         * <returns></returns>
+         */
+        public void botonCrear()
+        {
+
+        }
+
+        /**
+         * <summary>
+         * Verifica que todos los campos necesarios esten con valores correctos
+         * </summary>
+         * <param name=> </param>
+         * 
+         * <returns></returns>
+         */
+        public void verificarDatos()
+        {
+
+        }
+
+
+        /**
+         * <summary>
+         * refresca la tabla de la ventana padre.
+         * </summary>
+         * <param name=> </param>
+         * 
+         * <returns></returns>
+         */
+        public void refrescarTabla()
+        {
+
+        }
+    }
+}
+
