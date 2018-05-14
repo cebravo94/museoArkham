de870cf7390fe9affa39f0bcea30e6dfc698fc5b
raimundovu <rvasquez15@alumnos.utalca.cl>
2018-05-13 22:45:32 -0400

    Creación Controlador Agregar Usuario
    Se crea un controlador para la vista de creación de usuario.

--- MuseoArkham/Vista/Vistas Secretaria/VistaAgregarUsuario.Designer.cs
+++ MuseoArkham/Vista/Vistas Secretaria/VistaAgregarUsuario.Designer.cs
@@ -30,30 +30,30 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VistaAgregarUsuario));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxTipo = new System.Windows.Forms.ComboBox();
             this.botonCancelar = new System.Windows.Forms.Button();
             this.botonAceptar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.textBoxRut = new System.Windows.Forms.TextBox();
             this.textBoxCorreo = new System.Windows.Forms.TextBox();
-            this.textBoxContrasenha = new System.Windows.Forms.TextBox();
+            this.textBoxContrasenna = new System.Windows.Forms.TextBox();
             this.textBoxNombre = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.comboBoxTipo);
             this.groupBox1.Controls.Add(this.botonCancelar);
             this.groupBox1.Controls.Add(this.botonAceptar);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.textBoxRut);
             this.groupBox1.Controls.Add(this.textBoxCorreo);
-            this.groupBox1.Controls.Add(this.textBoxContrasenha);
+            this.groupBox1.Controls.Add(this.textBoxContrasenna);
             this.groupBox1.Controls.Add(this.textBoxNombre);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
@@ -65,6 +65,18 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Crear Usuario";
+            // 
+            // comboBoxTipo
+            // 
+            this.comboBoxTipo.FormattingEnabled = true;
+            this.comboBoxTipo.Items.AddRange(new object[] {
+            "Administrador Departamento",
+            "Gerente Exhibiciones",
+            "Bodeguero"});
+            this.comboBoxTipo.Location = new System.Drawing.Point(143, 213);
+            this.comboBoxTipo.Name = "comboBoxTipo";
+            this.comboBoxTipo.Size = new System.Drawing.Size(161, 21);
+            this.comboBoxTipo.TabIndex = 12;
             // 
             // botonCancelar
             // 
@@ -83,6 +95,18 @@
             this.botonAceptar.TabIndex = 10;
             this.botonAceptar.Text = "Aceptar";
             this.botonAceptar.UseVisualStyleBackColor = true;
+            this.botonAceptar.Click += new System.EventHandler(this.botonAceptar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(37, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tipo Usuario :";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // textBoxRut
             // 
@@ -98,12 +122,12 @@
             this.textBoxCorreo.Size = new System.Drawing.Size(161, 20);
             this.textBoxCorreo.TabIndex = 6;
             // 
-            // textBoxContrasenha
+            // textBoxContrasenna
             // 
-            this.textBoxContrasenha.Location = new System.Drawing.Point(143, 252);
-            this.textBoxContrasenha.Name = "textBoxContrasenha";
-            this.textBoxContrasenha.Size = new System.Drawing.Size(161, 20);
-            this.textBoxContrasenha.TabIndex = 5;
+            this.textBoxContrasenna.Location = new System.Drawing.Point(143, 252);
+            this.textBoxContrasenna.Name = "textBoxContrasenna";
+            this.textBoxContrasenna.Size = new System.Drawing.Size(161, 20);
+            this.textBoxContrasenna.TabIndex = 5;
             // 
             // textBoxNombre
             // 
@@ -152,29 +176,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre :";
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(37, 218);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(92, 16);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Tipo Usuario :";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Administrador Departamento",
-            "Gerente Exhibiciones",
-            "Bodeguero"});
-            this.comboBox1.Location = new System.Drawing.Point(143, 213);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(161, 21);
-            this.comboBox1.TabIndex = 12;
-            // 
             // VistaAgregarUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -203,10 +204,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBoxRut;
         private System.Windows.Forms.TextBox textBoxCorreo;
-        private System.Windows.Forms.TextBox textBoxContrasenha;
+        private System.Windows.Forms.TextBox textBoxContrasenna;
         private System.Windows.Forms.Button botonCancelar;
         private System.Windows.Forms.Button botonAceptar;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxTipo;
         private System.Windows.Forms.Label label5;
     }
 }
